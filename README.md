e1ab9792f00fac560af024c7e3aa953dd2405254
AdrianAnis <adriananis06@gmail.com>
2025-11-09 22:44:42 +0700

    First Commit for Sari Rasa

---
+++ README.md
@@ -0,0 +1,3 @@
+# smartify_application
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/smartify_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smartify_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
