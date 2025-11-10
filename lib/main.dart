14686ab1a12efe497f303cff110a8bb9a818c933
ahmad rafiansyah <ahmad rafiansyab20@gmail.com>
2025-11-10 12:15:26 +0700

    perubahan file main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Scaffold(body: Center()));
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Poppins'),
+    );
   }
 }
